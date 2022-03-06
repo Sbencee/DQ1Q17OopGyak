43e15bf0bfcabd94202a4c9f41a13489e309b16f
Sbencee <99654274+Sbencee@users.noreply.github.com>
2022-03-06 15:24:56 +0100

    Szorgalmi

---
+++ 4_gyakorlat_2_feladat_szorgalmi/src/gyak4/Gyak4_szorgalmi.java
@@ -0,0 +1,68 @@
+package gyak4;
+
+import java.util.Scanner;
+
+public class Gyak4_szorgalmi {
+
+	public static void main(String[] args) {
+		Szemely ember= new Szemely();
+		ember.setNev(nevBeolvas());
+		ember.setSuly(sulyBeolvas());
+		ember.setMagassag(magassagBeolvas());
+		System.out.println(ember.osszefuz());
+		
+		
+	}
+	static String nevBeolvas() {
+		String nevsulyTarol="";
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Kerem adjon meg egy nevet: ");
+		nevsulyTarol=sc.nextLine();
+		return nevsulyTarol;
+	}
+	static int sulyBeolvas() {
+		boolean ok=false;
+	    int sulyTarol = 0;
+	    Scanner sc = new Scanner(System.in);
+	    do{
+	    	try {
+	    		System.out.print("\nKérem a személy súlyát: ");
+	    		sulyTarol = sc.nextInt();
+	    		if (sulyTarol>0){
+	    			ok = true;
+	    		}
+	    		else {
+	    			ok=false;
+	    			System.out.println("\nHibás adat!");
+			   }
+	    		} catch (Exception e) {
+		   		System.out.println("\nHibás input! Számot adjon meg!");
+		   		sc.nextLine();//
+		   	}
+	    	}while(!ok);
+	   return sulyTarol;
+	}
+	static double magassagBeolvas() {
+		boolean ok=false;
+	    double magassagTarol = 0;
+	    Scanner sc = new Scanner(System.in);
+	    do{
+	    	try {
+	    		System.out.print("\nKérem a személy magasságát (pl: 1,92 ): ");
+	    		magassagTarol = sc.nextDouble();
+	    		if (magassagTarol>0){
+	    			ok = true;
+	    		}
+	    		else {
+	    			ok=false;
+	    			System.out.println("\nHibás adat!");
+			   }
+	    		} catch (Exception e) {
+		   		System.out.println("\nHibás input! Számot adjon meg!");
+		   		sc.nextLine();//
+		   	}
+	    	}while(!ok);
+	   return magassagTarol;
+	}
+
+}
